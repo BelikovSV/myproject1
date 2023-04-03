d3275915bce2f0978492010a18f93d7911a42bdc
BelikovSV <barstylers13@gmail.com>
2023-04-03 14:21:28 +0300

    add Dialogs

---
+++ Dialogs.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import s from './Dialogs.module.css'
+import {BrowserRouter} from "react-router-dom";
+
+const Dialogs = () => {
+    return (
+        <div className={s.dialogs}>
+            <div ClassName={s.dialog}>
+                <div>Alexandr</div>
+                <div>Roma</div>
+                <div>Kseina</div>
+                <div>Kate</div>
+            </div>
+            <div className={s.message}>
+                <div>Hi</div>
+                <div>My name is...</div>
+                <div>How old are you?</div>
+            </div>
+        </div>
+    );
+}
+
+export default Dialogs
